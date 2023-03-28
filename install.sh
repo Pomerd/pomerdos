d47feb379130e16832ba325e2709b41b12e9c162
Pomerd <93032674+Pomerd@users.noreply.github.com>
2023-03-28 17:29:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,7 @@ echo ""
 echo -e "\e[1;91m [+] Discord: \e[1;92mdiecord.gg/axid"
 echo -e "\e[1;91m [+] Telegram: \e[1;92mt.me/p0merdaga\e[1;97m"
 echo ""
-read -p " PomerdOS Sistemini Termux'a Kurmak İstiyor Musun?(Yes/No) : " input
-
-if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
-    clear
+read -p " PomerdOS Sistemini Termux'a Kuruluyor"
     cp bash.bashrc $HOME
     cd /data/data/com.termux/files/usr/etc
     rm -rf motd
@@ -23,9 +20,3 @@ if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
     echo -e "\e[1;91mYükleme Başarılı"
     echo -e "Termux'u Yeniden Başlat"
     exit  3
-elif [[ $input == No || $input == no || $input == n || $input == N ]]; then
-exit 2
-else
-echo -e "\e[1;91mYükleme Başarısız"
-exit 1
-fi
